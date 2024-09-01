5dfb3a439af80986094ea107a8eb7f9fea6b8f6c
Lars Rosenboom <mail@larsrosenboom.de>
2024-09-01 23:34:49 +0200

    Types.hpp aktualisieren

--- cilia/Types.hpp
+++ cilia/Types.hpp
@@ -1,14 +1,16 @@
 #include <cstdint>
+#include <cstddef>
+#include <limits>
 
 namespace cilia {
 
-    using Int = int;
+    using Int   = int;
     using Int8  = int8_t;
     using Int16 = int16_t;
     using Int32 = int32_t;
     using Int64 = int64_t;
 
-    using UInt = uint;
+    using UInt   = uint;
     using UInt8  = uint8_t;
     using UInt16 = uint16_t;
     using UInt32 = uint32_t;
@@ -17,10 +19,17 @@ namespace cilia {
     using Bool = bool;
     using Char = unsigned char;
 
-    using Float = float;
-    using Float16 = _Float16;
-    using Float32 = float;
-    using Float64 = double;
+    using Float    = float;
+    using Float16  = _Float16;
+    using Float32  = float;
+    using Float64  = double;
     using BFloat16 = __bf16;
+
+
+    const static Bool True = true;
+    const static Bool False = false;
+    const static nullptr_t NullPtr = nullptr;
+    const static Float32 NaN = std::numeric_limits<Float32>::quiet_NaN();
+    const static Float32 Infinity = std::numeric_limits<Float32>::infinity();
 
 }
