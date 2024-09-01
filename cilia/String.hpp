0d5e7accecb407b2c01788fa6fb0700c681262ad
Lars Rosenboom <mail@larsrosenboom.de>
2024-09-01 23:24:21 +0200

    String.hpp aktualisieren

--- cilia/String.hpp
+++ cilia/String.hpp
@@ -4,34 +4,6 @@
 #include <cmath>
 
 namespace cilia {
-    using Int = int;
-    using Int8  = int8_t;
-    using Int16 = int16_t;
-    using Int32 = int32_t;
-    using Int64 = int64_t;
-
-    using UInt = uint;
-    using UInt8  = uint8_t;
-    using UInt16 = uint16_t;
-    using UInt32 = uint32_t;
-    using UInt64 = uint64_t;
-
-    using Bool = bool;
-    using Char = unsigned char;
-
-    using Float = float;
-    using Float16 = _Float16;
-    using Float32 = float;
-    using Float64 = double;
-    using BFloat16 = __bf16;
-
-
-    const static Bool True = true;
-    const static Bool False = false;
-    const static nullptr_t NullPtr = nullptr;
-    const static Float32 NaN = std::numeric_limits<Float32>::quiet_NaN();
-    const static Float32 Infinity = std::numeric_limits<Float32>::infinity();
-
 
     class String : public std::string {
     public:
@@ -55,51 +27,8 @@ namespace cilia {
         }
 
     private:
+        // Hide the original snake_case functions
         using std::string::find_first_of;
     };
 
-    template <typename T, typename Allocator = std::allocator<T>>
-    class Array : public std::vector<T, Allocator> {
-    public:
-        using std::vector<T, Allocator>::vector;
-
-        auto pushBack(const T& element) {
-            std::string::push_back(element);
-        }
-
-        auto operator [](Int i) -> T& {
-            if (i < 0 || i >= this->size())
-                std::terminate();
-
-            return std::vector<T, Allocator>::operator[](i);
-        }
-
-    private:
-        using std::string::push_back;
-    };
-
-
-    auto printLineCil(const String& str) {
-        std::cout << str << std::endl;
-    }
-
-    auto printLineStd(const std::string& str) {
-        std::cout << str << std::endl;
-    }
-}
-
-
-int main() {
-    using namespace cilia;
-    using namespace std;
-
-    String strCil = "TestCil";
-    string strStd = "TestStd";
-
-    printLineStd(strCil);
-    printLineCil(strCil);
-    printLineStd(strStd);
-    printLineCil(strStd);
-
-    return 0;
 }
