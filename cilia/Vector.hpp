4b3901c10b2b28eb6c4b891247f2509587a81ee8
Lars Rosenboom <mail@larsrosenboom.de>
2024-09-02 09:20:57 +0200

    Update Vector.hpp

--- cilia/Vector.hpp
+++ cilia/Vector.hpp
@@ -6,10 +6,23 @@ namespace cilia {
     template <typename T, typename Allocator = std::allocator<T>>
     class Array : public std::vector<T, Allocator> {
     public:
+        using ValueType            = std::vector<T, Allocator>::value_type;
+        using TraitsType           = std::vector<T, Allocator>::traits_type;
+        using AllocatorType        = std::vector<T, Allocator>::allocator_type;
+        using Reference            = std::vector<T, Allocator>::reference;
+        using ConstReference       = std::vector<T, Allocator>::const_reference;
+        using Pointer              = std::vector<T, Allocator>::pointer;
+        using ConstPointer         = std::vector<T, Allocator>::const_pointer;
+        using Iterator             = std::vector<T, Allocator>::iterator;
+        using ConstIterator        = std::vector<T, Allocator>::const_iterator;
+        using ReverseIterator      = std::vector<T, Allocator>::reverse_iterator;
+        using ConstReverseIterator = std::vector<T, Allocator>::const_reverse_iterator;
+
+        // Alle Konstruktoren übernehemn
         using std::vector<T, Allocator>::vector;
 
         auto pushBack(const T& element) {
-            std::string::push_back(element);
+            std::vector<T, Allocator>::push_back(element);
         }
 
         auto operator [](Int i) -> T& {
@@ -22,7 +35,7 @@ namespace cilia {
 
     private:
         // Hide the original snake_case functions
-        using std::string::push_back;
+        using std::vector<T, Allocator>::push_back;
     };
 
 }
