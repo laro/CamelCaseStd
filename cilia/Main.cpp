94df0833092a86f7c8324a00b9c2559f618f1ee7
Lars Rosenboom <mail@larsrosenboom.de>
2024-09-02 09:06:52 +0200

    Update Main.cpp

--- cilia/Main.cpp
+++ cilia/Main.cpp
@@ -1,6 +1,16 @@
 #include <cilia/String.hpp>
 #include <cilia/Vector.hpp>
 
+namespace cilia {
+    auto printLineCil(const String& str) {
+        std::cout << str << std::endl;
+    }
+    
+    auto printLineStd(const std::string& str) {
+        std::cout << str << std::endl;
+    }
+}
+
 int main() {
     using namespace cilia;
     using namespace std;
