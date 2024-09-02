25ef9b2f20ab51776a3c22e43e7724ec5cbf548b
Lars Rosenboom <mail@larsrosenboom.de>
2024-09-02 09:34:28 +0200

    Update MainTest.cpp

--- MainTest.cpp
+++ MainTest.cpp
@@ -1,24 +1,21 @@
 #include <cilia/String.hpp>
 #include <cilia/Vector.hpp>
 
-namespace cilia {
-    auto printLineCil(const String& str) {
-        using namespace std;
-        cout << str << endl;
-    }
-    
-    auto printLineStd(const std::string& str) {
-        using namespace std;
-        cout << str << endl;
-    }
+using namespace cilia;
+
+auto printLineCil(const String& str) {
+    using namespace std;
+    cout << str << endl;
+}
+
+auto printLineStd(const std::string& str) {
+    using namespace std;
+    cout << str << endl;
 }
 
 int main() {
-    using namespace cilia;
-    using namespace std;
-
-    String strCil = "TestCil";
-    string strStd = "TestStd";
+    String      strCil = "TestCil";
+    std::string strStd = "TestStd";
 
     printLineStd(strCil);
     printLineCil(strCil);
