67fd8e2fc08f70de0fbb5baa6bfb4dff05fc4196
Lars Rosenboom <mail@larsrosenboom.de>
2024-09-03 08:49:16 +0200

    Added *.xcodeproj and adapted code to Clang.

---
+++ CamelCaseStd/CamelCaseStd/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CamelCaseStd
+//
+//  Created by Lars Rosenboom on 03.09.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
