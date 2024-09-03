db612e897a5e047d6acaf34cb80cb5bfaa40bd58
laro <larsrosenboom@gmail.com>
2024-09-03 09:28:30 +0200

    Update

--- cilia/Macros.h
+++ cilia/Macros.h
@@ -1,10 +1,15 @@
 #pragma once
 
+// Deactivated, because I (currently) do not want to pollute the global namespace
+// with macros.
+
 //TODO I'd like to have a keyword "var" for type inferring in variable declarations,
 //     for "var i = 0", because IMHO it is clearer than the ubiquitous "auto".
-//#define var  auto
+//#define var auto
 
 //TODO I'd like to have a keyword "func" for function declarations,
 //     for "func isOpen() -> Bool { ... }", as to avoid the "most vexing parse",
-//     abd because IMHO it is clearer than the ubiquitous "auto".
+//     and because IMHO it is clearer than the ubiquitous "auto".
 //#define func auto
+
+
